825aec090996a9469b0e8e08a57218e7b5bea39f
Manoel Eric <manuel@box3.work>
2024-12-09 19:04:46 -0300

    edit a api host

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const PRODUCTION_API_URL = `${window.location.protocol}//${window.location.hostname}:5000/`
+export const PRODUCTION_API_URL = `http://46.202.153.202:5000`
 
 const DEVELOPMENT_API_URL = 'http://localhost:5000'
 
